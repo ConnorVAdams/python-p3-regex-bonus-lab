301d423e05d507ad70087142ffb9545da0249136
Connor Adams <connorvadams@gmail.com>
2023-10-25 21:40:19 -0600

    Submits lab.

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,15 @@
 import re
 
-my_pattern = r""
+my_pattern = r"[A-Z]['\w\s]*day[,\s'\w\s]*[?.]"
 my_regex = re.compile(my_pattern)
 
+FINDALL_STRING = """
+    It's such a lovely day today.
+    Where'd all the time go?
+    Some weather we're having today, huh?
+    Tomorrow never knows!
+    Maybe today's just not my day.
+    It's clobbering time!
+"""
+
+print(my_regex.findall(FINDALL_STRING))
